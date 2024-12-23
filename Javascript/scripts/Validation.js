15ebe2d399a79df551601ca0fd3414d22148252e
vinaneosoft <vinaneosoft@gmail.com>
2024-12-23 17:47:16 +0530

    validation

--- Javascript/scripts/Validation.js
+++ Javascript/scripts/Validation.js
@@ -1,9 +1,18 @@
 const nameNode=document.getElementById("firstname");
 const nameErrorNode=document.getElementById("nameError");
 nameNode.addEventListener("keyup", ()=>validateEmployeeName())
+const namePattern=new RegExp("^[A-Ba-z ]*$")
+
 function validateEmployeeName(){
     nameErrorNode.textContent="";
     // required validation
-    if(nameNode.value=="")
+    const empname=nameNode.value;
+    if(empname=="")
         nameErrorNode.textContent="name is required";    
+    else if(namePattern.test(empname)==false)
+        nameErrorNode.textContent="Please enter valid name";   
+    else if(empname.length<2)
+        nameErrorNode.textContent="name must be more than 1 character long...";    
+   
 }
+
